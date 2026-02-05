6e3f91c4d9c4fbe72f076c2419588969c1cefce5
andrew <andrewalangm@gmail.com>
2026-02-05 13:14:50 -0600

    Handle event: drawing a card

--- lib/p_phoenix_liveview_course_web/live/blackjack_live.ex
+++ lib/p_phoenix_liveview_course_web/live/blackjack_live.ex
@@ -9,12 +9,58 @@ defmodule PPhoenixLiveviewCourseWeb.BlackjackLive do
      layout: {PPhoenixLiveviewCourseWeb.Layouts, :game}}
   end
 
+  @impl true
+  def handle_event("draw", %{"count" => count}, socket) do
+    {:noreply, socket |> draw_card(count) |> cpu_draw_card(count) |> handle_winner_on_draw()}
+  end
+
+  # Privates
   defp init_deck(socket) do
-    socket |> assign(cards: @cards, player: [], cpu: [])
+    socket |> assign(cards: @cards, player: [], cpu: [], winner: nil)
   end
 
   defp first_deal(socket) do
     [card1, card2, card3, card4] = Enum.take_random(@cards, 4)
     socket |> assign(player: [card1, card2], cpu: [card3, card4])
+  end
+
+  defp points(cards) do
+    Enum.reduce(cards, 0, fn card, acc -> acc + card end)
+  end
+
+  defp draw_card(socket, count) do
+    if points(socket.assigns.player) < 21 do
+      [card1] = Enum.take_random(@cards, count)
+      new_player_cards = [card1 | socket.assigns.player]
+
+      socket |> assign(player: new_player_cards)
+    else
+      socket |> put_flash(:error, "Cannot take another card")
+    end
+  end
+
+  defp cpu_draw_card(socket, count) do
+    if points(socket.assigns.cpu) < 17 do
+      [card1] = Enum.take_random(@cards, count)
+      new_cpu_cards = [card1 | socket.assigns.cpu]
+
+      socket |> assign(cpu: new_cpu_cards)
+    else
+      socket
+    end
+  end
+
+  defp handle_winner_on_draw(socket) do
+    player_points = points(socket.assigns.player)
+    cpu_points = points(socket.assigns.cpu)
+
+    winner =
+      cond do
+        player_points > 21 -> :cpu
+        cpu_points > 21 -> :player
+        true -> nil
+      end
+
+    socket |> assign(winner: winner)
   end
 end
