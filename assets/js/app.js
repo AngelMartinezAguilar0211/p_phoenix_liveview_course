8e1d4d32ee8da9e7000c1680279b6a24cdee9221
andrew <andrewalangm@gmail.com>
2026-02-24 15:55:41 -0600

    Fix duplicated variable in app js

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,13 +18,10 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 import PokemonBattle from "./pokemon.hooks";
-
-let Hooks = {};
-Hooks.PokemonBattle = PokemonBattle;
 
 let Hooks = {};
 Hooks.PokemonBattle = PokemonBattle;
@@ -34,9 +31,9 @@ let csrfToken = document
   .getAttribute("content");
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken},
-  hooks: Hooks
-})
+  params: { _csrf_token: csrfToken },
+  hooks: Hooks,
+});
 
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
@@ -50,4 +47,4 @@ liveSocket.connect();
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
+window.liveSocket = liveSocket;
